4c00a2d4430ab9037e1f1bbeccef9cfdda550a12
Tatyana <tatyanakorabelnikovamama@gmail.com>
2025-01-23 16:47:01 +0200

    add initial files

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 
 const API_KEY = '48210883-f70532ba3d786d958e1d4920f'; 
 const BASE_URL = 'https://pixabay.com/api/';
@@ -16,9 +19,22 @@ export async function fetchImages(query, page = 1, perPage = 15) {
     };
 
     const response = await axios.get(BASE_URL, { params });
+   // Якщо API повертає некоректні дані
+    if (!response.data.hits || response.data.hits.length === 0) {
+      iziToast.info({
+        title: 'No Results',
+        message: 'No images found for your query. Please try again.',
+      });
+      return null;
+    }
+
     return response.data;
   } catch (error) {
-    console.error('Error fetching images:', error);
-    throw error;
+    // Обробка помилок і дружнє повідомлення
+    iziToast.error({
+      title: 'Error',
+      message: 'Something went wrong. Please try again later.',
+    });
+    throw error; // Повторно кидаємо помилку для подальшої обробки
   }
 }
